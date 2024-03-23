27d2e566babf7a9028376b4fd112b7035e5ec0cd
Ksandr <MYEMAIL>
2024-03-23 12:23:24 +0700

    Fixed menu style

--- calculator.frontend/src/Navbar.jsx
+++ calculator.frontend/src/Navbar.jsx
@@ -1,21 +1,24 @@
-import { Link } from 'react-router-dom'
+import './Navbar.css'
+import { NavLink } from 'react-router-dom'
 import { MENU } from './scripts/const.js'
 
 export function Navbar() {
     return (
         <>
-            <Link
+            <NavLink
                 to={MENU.calculator.address}
                 state={{ pageName: MENU.calculator.name }}
+                className={({ isActive }) => isActive ? "active-navbar-item" : ""}
             >
                 <button>{MENU.calculator.name}</button>
-            </Link>
-            <Link
+            </NavLink>
+            <NavLink
                 to={MENU.history.address}
                 state={{ pageName: MENU.history.name }}
+                className={({ isActive }) => isActive ? "active-navbar-item" : ""}
             >
                 <button>{MENU.history.name}</button>
-            </Link>
+            </NavLink>
         </>
     )
 }
