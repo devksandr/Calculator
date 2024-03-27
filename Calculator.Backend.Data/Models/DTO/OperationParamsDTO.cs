750be5bf174f15de39c65fd59a67e43544d66528
Ksandr <MYEMAIL>
2024-03-27 21:39:33 +0700

    Added calculator params validation on Backend

--- Calculator.Backend.Data/Models/DTO/OperationParamsDTO.cs
+++ Calculator.Backend.Data/Models/DTO/OperationParamsDTO.cs
@@ -7,7 +7,11 @@ namespace Calculator.Backend.Data.Models.DTO
 {
     public class OperationParamsDTO
     {
+        private const int NUMBER_LIMIT = 1_000_000_000;
+
+        [Range(-NUMBER_LIMIT, NUMBER_LIMIT)]
         public double Param1 { get; set; }
+        [Range(-NUMBER_LIMIT, NUMBER_LIMIT)]
         public double Param2 { get; set; }
         [JsonConverter(typeof(OperationTypeConverter))]
         public OperationType OperationType { get; set; }
