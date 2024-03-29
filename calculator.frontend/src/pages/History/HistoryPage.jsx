97f0ba0e76721c5c9273997615a3be3196443db5
Ksandr <MYEMAIL>
2024-03-29 15:02:05 +0700

    Added Ping on History page

--- calculator.frontend/src/pages/History/HistoryPage.jsx
+++ calculator.frontend/src/pages/History/HistoryPage.jsx
@@ -1,10 +1,12 @@
 import { useState, useEffect } from 'react'
+import { useInterval } from '../../hooks/useInterval'
 import axios from 'axios'
 import { API_URI } from '../../scripts/const.js'
 import HistoryTable from '../../components/HistoryTable/HistoryTable.jsx';
 import { useOutletContext } from "react-router-dom";
 import { Preloader } from '../../components/Preloader/Preloader.jsx'
 import { getConnectionErrorByStatus, WARNING_MESSAGES } from '../../scripts/connectionMessageService.js'
+import { pingBackendRequest, PING_TIMER } from '../../scripts/backendPingService'
 
 export function HistoryPage({ header }) {
 	const [loading, setLoading] = useOutletContext();
@@ -14,6 +16,36 @@ export function HistoryPage({ header }) {
 	useEffect(() => {
 		handleGetHistoryRequest();
 	}, []);
+
+	useInterval(() => {
+		handlePingRequest();
+	}, PING_TIMER);
+
+	const handlePingRequest = async () => {
+		let pingResult = await pingBackendRequest();
+
+		if (!pingResult.server) {
+			setLoading({
+				...loading,
+				data: false,
+				message: WARNING_MESSAGES.server
+			});
+		}
+		else if (!pingResult.database) {
+			setLoading({
+				...loading,
+				data: false,
+				message: WARNING_MESSAGES.database
+			});
+		}
+		else {
+			setLoading({
+				...loading,
+				data: true,
+				message: ''
+			});
+		}
+	};
 
 	function handleGetHistoryRequest() {
 		axios.get(`${API_URI}/History`)
